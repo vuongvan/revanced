4b6dcf0d21481ce2391747c0c0a0b471a3985f3f
Fei Yu <maivanvuong@gmail.com>
2023-08-12 20:24:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -164,11 +164,11 @@ fi
 
 if youtube_t=$(toml_get_table "YouTube"); then youtube_mode=$(toml_get "$youtube_t" "build-mode") || youtube_mode="apk"; else youtube_mode="module"; fi
 if music_t=$(toml_get_table "Music"); then music_mode=$(toml_get "$music_t" "build-mode") || music_mode="apk"; else music_mode="module"; fi
-if [ "$youtube_mode" != apk ] && [ "$music_mode" != module ]; then
+if [ "$youtube_mode" != module ] || [ "$music_mode" != module ]; then
 	log "\nInstall [Vanced Microg](https://github.com/TeamVanced/VancedMicroG/releases) for non-root YouTube or YT Music"
 fi
-#log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)"
-log "\nChangelog:\n---"
+log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)"
+log "---\nChangelog:"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
 pr "Done"
